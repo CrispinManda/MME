32ba2e8d7442e904313eaa175fb32f7c585a1055
Crispin Manda <crispinmanda06@gmail.com>
2024-11-22 19:23:40 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MME</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <?php echo 'hello' ?> ;<br>
+    <!-- variables -->
+     <?php
+     $x = 20;
+     echo 'The answer is'  .  ' '. $x + 2 ;' <br>';
+
+
+     $y = 'John';
+     echo '<br>' . $y
+
+     ?>
+    <!-- how to declare and use a variable  -->
+
+    <!-- data types -->
+     <!-- string -->
+      <!-- number
+      boolean
+      array -->
+
+
+
+</body>
+</html>
